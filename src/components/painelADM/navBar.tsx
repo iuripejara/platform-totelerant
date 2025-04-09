db893af8a0a4cd3eb6ab467abb3b2af47d8b9755
iuripejara <167295209+iuripejara@users.noreply.github.com>
2025-04-09 21:31:14 +0100

    21:30
    menu lateral

--- src/components/painelADM/navBar.tsx
+++ src/components/painelADM/navBar.tsx
@@ -1,11 +1,21 @@
+"use client";
 import Image from "next/image";
 import { Aldrich } from "next/font/google";
+import { useState } from "react";
 
 const aldrich = Aldrich ({
     weight: "400",
     subsets: ["latin"]
 })
+
+
+
+
 export default function NavBar() {
+    const [lateralNave,setLateralNave] = useState(false)
+    const clicNave = () => {
+        setLateralNave(!lateralNave)
+    }
     return(
         <>
             <nav className="
@@ -13,8 +23,38 @@ export default function NavBar() {
                 md:justify-around
             ">
                 <button>
-                    <Image src="ambuger.svg" alt="menul ambuger" width={25} height={21} />
+                    <Image 
+                        src="ambuger.svg" 
+                        alt="menul ambuger" 
+                        width={25} height={21}
+                        onClick={clicNave}
+                    />
                 </button>
+                { lateralNave && (
+                    <div className="
+                            fixed left-0 top-0 h-full w-80 pt-1 bg-zinc-800 shadow-lg
+                            transition-transform duration-150
+                        ">
+                        <div className="flex justify-between">
+                            <h1 className="
+                                text-xl text-zinc-100 ml-5 my-9
+                            
+                            ">
+                                Menu
+                            </h1>
+                            <button 
+                                className="text-zinc-100 mr-5 text-2xl"
+                                onClick={clicNave}
+                            >
+                                X
+                            </button>
+                        </div>
+                        
+                        <hr />
+                    </div>
+                )
+
+                }
                 <h1 className={` text-2xl text-zinc-50 ${aldrich.className}`}>Perfil do adm</h1>
                 <button>
                     <Image src="luaBranca.svg" alt="menul ambuger" width={32} height={32} />
