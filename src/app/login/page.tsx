adb388b8dc24d74d45c5eacf828b1f4fac1044ee
iuripejara <167295209+iuripejara@users.noreply.github.com>
2025-05-19 22:58:09 +0100

    19/05/25 22:57
    melhorias login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -24,17 +24,18 @@ export default function Login() {
 
     return(
         <div className="relative h-screen bg-bg-img bg-cover bg-center">
-            <div className="absolute bottom-0 flex items-center justify-center   ">
+            <div className="absolute bottom-0 flex items-center justify-center md:bottom-60 md:w-1/2 left-1/2 transform -translate-x-1/2 ">
                 <div 
                     className="
                         bg-azulBg  rounded-t-2xl w-screen
                         shadow-lg p-9 text-zinc-100
+                        md:rounded-md
                         "
                     >
                     <header className={`text-center text-3xl font-bold mb-6 ${aldrich.className}`}>Tolerantlevel</header>
                     <div>
                         <FormularioLogin/>
-                        <footer className=" text-center text-[11px] mt-6 flex justify-between">
+                        <footer className=" text-center text-[11px] md:text-[15px] mt-6 flex justify-between">
                             <span>©Todos os direitos reservado.Tolerantlevel 2025</span>
                             <span><a href="https://github.com/iuripejara">PrestesDev</a></span>
                         </footer>
