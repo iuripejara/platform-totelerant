4549b00d7687c0b549e2579c7d4920fa87df72af
iuripejara <167295209+iuripejara@users.noreply.github.com>
2025-08-30 11:12:48 +0100

    Update GeradorDeSenhas.tsx

--- src/components/geradorDesenha/GeradorDeSenhas.tsx
+++ src/components/geradorDesenha/GeradorDeSenhas.tsx
@@ -1,15 +1,8 @@
-import { useState } from "react";
-
 interface BotaoAleatorio{
     className:  string,
 
 }
-
 export default function GeradorDeSenhas({className}:BotaoAleatorio) {
-
-
-    
-
     return(
         <>
             <button className={className}>
